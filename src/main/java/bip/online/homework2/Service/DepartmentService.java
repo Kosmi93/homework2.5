cad9d0a778cddfc252ca8d5c2eb2aa8cedbf029d
Kosmi93 <k.evrei@mail.ru>
2024-03-01 22:51:00 +0300

    Commit homework2.8

--- src/main/java/bip/online/homework2/Service/DepartmentService.java
+++ src/main/java/bip/online/homework2/Service/DepartmentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,10 +16,11 @@ public class DepartmentService {
     public DepartmentService(EmployeeService service) {
         this.service = service;
     }
-    public List<Employee> getAll(){
+
+    public Map<String,List<Employee>> getAll(){
         return service.getAll().stream()
                 .sorted(Comparator.comparing(Employee::getDepartment))
-                .collect(Collectors.toList());
+                .collect(Collectors.groupingBy(Employee::getDepartment));
     }
 
     public List<Employee> getByDepartment(String departmentId) {
--- src/main/java/bip/online/homework2/controller/DepartmentController.java
+++ src/main/java/bip/online/homework2/controller/DepartmentController.java
@@ -3,13 +3,11 @@ package bip.online.homework2.controller;
 import bip.online.homework2.Service.DepartmentService;
 import bip.online.homework2.entity.Employee;
 import bip.online.homework2.exception.EmployeeNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("department")
@@ -21,20 +19,27 @@ public class DepartmentController {
     }
 
     @GetMapping("/max-salary")
-    public Employee maxSalary(@RequestParam String departmentId){
+    public Employee maxSalary(@RequestParam String departmentId) {
 
         return service.maxSalary(departmentId);
     }
 
     @GetMapping("/min-salary")
-    public Employee minSalary(@RequestParam String departmentId){
+    public Employee minSalary(@RequestParam String departmentId) {
         return service.minSalary(departmentId);
     }
 
     @GetMapping("/all")
-    public List<Employee> allDepartment(@RequestParam(defaultValue = "default") String departmentId){
+    public Map<String,List<Employee>> allDepartment() {
 
-        return departmentId.equals("default")?service.getAll():service.getByDepartment(departmentId);
+        // return departmentId.equals("default")?service.getAll():service.getByDepartment(departmentId);
+        return  service.getAll() ;
+    }
+    @GetMapping("/all-{id}")
+    public List<Employee> findByDepartment(@PathVariable String id) {
+
+         return service.getByDepartment(id);
+
     }
 
 }
